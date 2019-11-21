ddf056073f628aee490ea06865b6b4e2d8bea765
DT-HC <54913222+DT-HC@users.noreply.github.com>
2019-11-21 14:19:08 -0600

    Added the baseline for interactions of the Worker class with the Cell class

--- cs30-p5js-template-master/sketch.js
+++ cs30-p5js-template-master/sketch.js
@@ -7,20 +7,28 @@
 
 
 let grid = [];
-let W = 50
+let W = 50;
+let worker;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+  
   for (let i = 0; i < windowWidth; i += W){
     for (let j = 0; j < windowHeight; j += W)
       grid.push(new Cells(i,j,W));
   }
+  
+   worker = new Worker
   }
 
 function draw() {
+  translate(W/2, W/2)
   for (let i = 0; i < grid.length; i++){
     grid[i].display()
   }
+  worker.display()
+  worker.move()
   
   
 
@@ -34,10 +42,10 @@ class Cells {
     this.width = Cellw;
 }
   display() {
-    if (((mouseX < this.x + this.width && mouseX > this.x) && (mouseY > this.y && mouseY < this.width + this.y)) && mouseIsPressed){
+    if ((dist(worker.x, worker.y, this.x, this.y) < W/2) && mouseIsPressed){
       fill(0)
   }
-  else {
+    else {
     fill(220)
   }
     rect(this.x, this.y, this.width, this.width)
@@ -47,13 +55,42 @@ class Cells {
 }
 
 class Worker {
+  constructor() {
+    this.width = 4;
+    this.length = 8;
+    this.x = 110;
+    this.y = 50;
+    this.xLoc = floor(this.x/W)
+    this.yLoc = floor(this.y/W)
+    this.stepSize = 2;
+  }
+
+  display() {
+    fill(0)
+    rect(this.x,this.y, this.width, this.length)
+  }
+  move() {
+    let choice = random(100);
+    if (choice < 25) {
+      //up
+      this.y -= this.stepSize;
+    }
+    else if (choice < 50) {
+      //down
+      this.y += this.stepSize;
+    }
+    else if (choice < 75) {
+      //left
+      this.x -= this.stepSize;
+    }
+    else {
+      //right
+      this.x += this.stepSize;
+    
+  }
 
 }
-
-class Queen {
-
 }
-
 
 
 
