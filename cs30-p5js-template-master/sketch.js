1e68ae2f2bd175c1454419f20d8952073d15744d
DT-HC <54913222+DT-HC@users.noreply.github.com>
2019-11-21 22:18:20 -0600

    Update sketch.js

--- cs30-p5js-template-master/sketch.js
+++ cs30-p5js-template-master/sketch.js
@@ -4,12 +4,24 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
-
+let rWidth;
+let rY;
+let rHeight;
+let seed = 0;
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rectMode(BOTTOM);
+  rWidth = 5;
+  rY = 5;
 }
 
 function draw() {
   background(220);
+  fill(0);
+  createOcean()
+}
+
+function createOcean() {
+  for (let i = 0; i < rWidth; i+rWidth)
+  console.log(i)
 }
