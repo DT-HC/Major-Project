4f9533fd402b4b96d9a220882bab6195c4b461d6
DT-HC <54913222+DT-HC@users.noreply.github.com>
2019-12-05 14:18:59 -0600

    Added in a seprate .js file for procedural Map generation

--- cs30-p5js-template-master/demo.js
+++ cs30-p5js-template-master/demo.js
@@ -7,7 +7,7 @@
 
 
 let grid = [];
-let W = 50;
+let W = 100;
 let worker;
 
 function setup() {
@@ -17,11 +17,13 @@ function setup() {
   
   for (let i = 0; i < windowWidth; i += W){
     for (let j = 0; j < windowHeight; j += W)
-      grid.push(new Cells(i,j,W)); 
+      grid.push(new Cells(i,j,W));
+      grid 
   }
-  worker = new Worker(100,100);
-  dworker = new Worker(100, 200);
-
+  
+   worker = new Worker(10,10);
+   dworker = new Worker(500,100);
+   
   }
 
 function draw() {
@@ -41,12 +43,12 @@ function draw() {
 
 
 class Cells {
-  constructor(Cellx, Celly,Cellw,){
+  constructor(Cellx, Celly,Cellw){
     this.x = Cellx;
     this.y = Celly;
     this.width = Cellw;
     this.fill = 220;
-    this.state = "yes"
+    this.state = ""
 }
   display() {
     if ((dist(dworker.x, dworker.y, this.x, this.y) < W/2) || (dist(worker.x, worker.y, this.x, this.y) < W/2)){
@@ -73,8 +75,7 @@ class Worker {
     this.xLoc = floor(this.x/W)
     this.yLoc = floor(this.y/W)
     this.stepSize = 2;
-    this.a = 90;
-    this.choice = 1;
+    this.a = 90
   }
 
   display() {
@@ -90,20 +91,19 @@ class Worker {
   move() {
     push()
     translate(this.x , this.y);
-     this.choice = random(1,4)
     for (let i = 0; i < grid.length; i++) {
-      if (this.xLoc === grid[i].x && this.yLoc === grid[i].y){
-        // let choice = random(1,4);
-        if (this.choice = 1) {
+      if (dist(this.x, this.y, grid[i].x, grid[i].y) < W/2){
+        let choice = random(1,4);
+        if (choice = 1) {
           this.a = atan2((grid[i].y + W) - this.y, (grid[i].x) - this.x)
         }
-        else if (this.choice = 2) {
+        else if (choice = 2) {
           this.a = atan2((grid[i].y - W) - this.y, (grid[i].x) - this.x)
         }
-        else if (this.choice = 3) {
+        else if (choice = 3) {
           this.a = atan2((grid[i].y) - this.y, (grid[i].x + W) - this.x)
         }
-        else if (this.choice = 4) {
+        else if (choice = 4) {
           this.a = atan2((grid[i].y) - this.y, (grid[i].x - W) - this.x)
         }
 
